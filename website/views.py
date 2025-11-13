228f394b3f820c10ca9921a0500b3945b82e8a91
Johan <carljohan.modin@gmail.com>
2025-11-13 12:22:11 +0100

    New design (#1)
    * Added tech stack icons
    * Restructured files
    * Added icon views for Django rendering

--- website/views.py
+++ website/views.py
@@ -4,5 +4,45 @@ from django.http import FileResponse
 import plotly.express as px
 
 def home(request):
+    tech_categories = [
+        {
+            "title": "Languages",
+            "icons": [
+                {"name": "C++", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"},
+                {"name": "C", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"},
+                {"name": "C#", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"},
+                {"name": "Python", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"},
+                {"name": "Bash", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg"},
+                {"name": "HTML5", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"},
+            ],
+        },
+        {
+            "title": "Tools",
+            "icons": [
+                {"name": "Git", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"},
+                {"name": "CMake", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cmake/cmake-original.svg"},
+                {"name": "Docker", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"},
+                {"name": "Linux", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg"},
+                {"name": "VS Code", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"},
+                {"name": "ChatGPT", "src": "https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg"},
+            ],
+        },
+        {
+            "title": "Embedded",
+            "icons": [
+                {"name": "Arduino", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original.svg"},
+                {"name": "Raspberry Pi", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/raspberrypi/raspberrypi-original.svg"},
+                {"name": "PlatformIO", "src": "https://cdn.simpleicons.org/platformio/F5822B"},
+            ],
+        },
+        {
+            "title": "DevOps",
+            "icons": [
+                {"name": "Azure", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg"},
+                {"name": "Kubernetes", "src": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg"},
+                {"name": "MySQL", "src": "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"},
+            ],
+        },
+    ]
 
-    return render(request, 'website/home.html')
+    return render(request, 'website/home.html', {"tech_categories": tech_categories})
